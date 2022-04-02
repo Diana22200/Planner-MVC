9eb84b8c01ff0506655c8d1cd9e36ef11363e79e
Stephanny Diana Alejandra Guevara Bocanegra <sdguevara85@misena.edu.co>
2022-04-02 10:51:59 -0500

    Se arreglaron las rutas

--- controllers/Usuarios.php
+++ controllers/Usuarios.php
@@ -1,5 +1,19 @@
 <?php
-public function index(){
+class UsuariosController{
+    public function __construct(){
+        session_start();
+        require_once "models/UsuariosModel.php";
+    }
+    public function index(){
+        $usuarios = new Usuarios_model();
+        $num_doc = $_SESSION['numero_docu'];
+        $name_doc = $_SESSION['tipo_docu'];
+        $data["perfiles"] = $usuarios->get_perfiles($num_doc,$name_doc);
+
+        require_once "views/usuarios/perfil_admin.php";
+    }
+    public function get_perfil(){
     require_once "views/usuarios/usuarios.php";
+    }
 }
 ?>
